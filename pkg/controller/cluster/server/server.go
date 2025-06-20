15453969395af8e6b3164c819d723bd51d8d065d
muicoder <muicoder@gmail.com>
2025-06-20 14:16:20 +0800

    AIO
    build

--- pkg/controller/cluster/server/server.go
+++ pkg/controller/cluster/server/server.go
@@ -100,31 +100,31 @@ func (s *Server) podSpec(ctx context.Context, image, name string, persistent boo
 			{
 				Name: "run",
 				VolumeSource: v1.VolumeSource{
-					EmptyDir: &v1.EmptyDirVolumeSource{},
+					EmptyDir: &v1.EmptyDirVolumeSource{Medium: v1.StorageMediumMemory},
 				},
 			},
 			{
 				Name: "varrun",
 				VolumeSource: v1.VolumeSource{
-					EmptyDir: &v1.EmptyDirVolumeSource{},
+					EmptyDir: &v1.EmptyDirVolumeSource{Medium: v1.StorageMediumMemory},
 				},
 			},
 			{
 				Name: "varlibcni",
 				VolumeSource: v1.VolumeSource{
-					EmptyDir: &v1.EmptyDirVolumeSource{},
+					EmptyDir: &v1.EmptyDirVolumeSource{Medium: v1.StorageMediumMemory},
 				},
 			},
 			{
 				Name: "varlog",
 				VolumeSource: v1.VolumeSource{
-					EmptyDir: &v1.EmptyDirVolumeSource{},
+					EmptyDir: &v1.EmptyDirVolumeSource{Medium: v1.StorageMediumMemory},
 				},
 			},
 			{
 				Name: "varlibkubelet",
 				VolumeSource: v1.VolumeSource{
-					EmptyDir: &v1.EmptyDirVolumeSource{},
+					EmptyDir: &v1.EmptyDirVolumeSource{Medium: v1.StorageMediumMemory},
 				},
 			},
 		},
